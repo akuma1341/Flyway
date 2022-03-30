63c179b9748b02546eb4036b370720140ec04cb6
Raman Rashchupkin <rrashchupkin@piesoft.us>
2022-03-30 16:27:08 +0300

    Initial commit

---
+++ src/main/java/com/example/flywaytask/FlywayTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.flywaytask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlywayTaskApplication.class, args);
+	}
+
+}
